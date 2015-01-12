df24323aa6fec8a723f94bbfbe2a86d0476eabd3
Fernando Morales <fmorales@localhost.localdomain>
2015-01-12 17:52:19 -0300

    Add grids

--- src/Layers/Grids/Grid.js
+++ src/Layers/Grids/Grid.js
@@ -1,10 +1,8 @@
 CityDashboard.Grid = function( grid_params, attr, map ){
 
-  // map is a GoogleMap Object
-  // map.map is the actual map
 
   if ( attr.type === 'square' )
-    CityDashboard.SquareGrid( grid_params, attr, map.map );
+    CityDashboard.SquareGrid( grid_params, attr, map );
 
 };
 
--- src/Layers/Grids/SquareGrid.js
+++ src/Layers/Grids/SquareGrid.js
@@ -1,34 +1,57 @@
 CityDashboard.SquareGrid = function( grid_params, attr, map ){
 
-  var square;
-  var square2;
+  google.maps.event.addDomListener(window, 'load',function () {
 
-  var myLatlng = [
-    new google.maps.LatLng(parseFloat(grid_params.lat), parseFloat(grid_params.lng)),
-    new google.maps.LatLng(parseFloat(grid_params.lat)+0.01, parseFloat(grid_params.lng)),
-    new google.maps.LatLng(parseFloat(grid_params.lat)+0.01, parseFloat(grid_params.lng)+0.01),
-    new google.maps.LatLng(parseFloat(grid_params.lat), parseFloat(grid_params.lng)+0.01),
-    new google.maps.LatLng(parseFloat(grid_params.lat), parseFloat(grid_params.lng))
-  ];
+    var mapBounds = map.getBounds();
 
-  square = new google.maps.Polygon({
-    paths: myLatlng,
-    strokeColor: attr.color || 'blue',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: attr.color || 'blue',
-    fillOpacity: 0.2,
-    clickable:true
-  });
+    var size = attr.size || 0.03
 
-  square.setMap(map);
+    //var x = parseFloat(grid_params.lat);
+    //var y = parseFloat(grid_params.lng);
 
-  google.maps.event.addListener(square, 'mouseover', function(event) {
-    square.setOptions({fillOpacity: 0.4});
-  });
+    var NE = mapBounds.getNorthEast();
+    var SW = mapBounds.getSouthWest();
 
-  google.maps.event.addListener(square, 'mouseout', function(event) {
-    square.setOptions({fillOpacity: 0.2});
+    var h = Math.ceil(Math.abs(SW.lat()-NE.lat())/size);
+    var w = Math.ceil(Math.abs(SW.lng()-NE.lng())/size);
+
+    for (var i = 0; i < h; i++) {
+      for (var j = 0; j < w; j++) {
+
+        var x = NE.lat() - size*i;
+        var y = NE.lng() - size*j;
+
+        var myLatlng = [
+          new google.maps.LatLng(x, y),
+          new google.maps.LatLng(x-size, y),
+          new google.maps.LatLng(x-size, y-size),
+          new google.maps.LatLng(x, y-size),
+          new google.maps.LatLng(x, y)
+        ];
+
+        var square = new google.maps.Polygon({
+          paths: myLatlng,
+          strokeColor: attr.color || 'blue',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: attr.color || 'blue',
+          fillOpacity: 0.2,
+          clickable:true
+        });
+
+        google.maps.event.addListener(square, 'mouseover', function(event) {
+            this.setOptions({fillOpacity: 0.4});
+          });
+
+        google.maps.event.addListener(square, 'mouseout', function(event) {
+            this.setOptions({fillOpacity: 0.2});
+          });
+
+        square.setMap(map);
+
+      }
+    }
+
   });
 
 };
