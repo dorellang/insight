b6a1f53a119803614bd5ece008634aaa5771dda4
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-13 17:12:48 -0300

    Cleanup code of MarkerSelector.js

--- src/Layers/Markers/MarkerSelector.js
+++ src/Layers/Markers/MarkerSelector.js
@@ -1,15 +1,15 @@
-function MarkerSelector( layer_params, attr, map, assoc_layer ){
+function MarkerSelector(layer_params, attr, map, assoc_layer) {
 
-  if ( attr.type === 'circle' )
-    return new CityDashboard.CircleMarker( layer_params, attr, map, assoc_layer );
+    if (attr.type === 'circle')
+        return new CityDashboard.CircleMarker(layer_params, attr, map, assoc_layer);
 
-  else if ( attr.type === 'image' )
-    return new CityDashboard.ImageMarker( layer_params, attr, map, assoc_layer );
+    else if (attr.type === 'image')
+        return new CityDashboard.ImageMarker(layer_params, attr, map, assoc_layer);
 
-  else if ( attr.type === 'polyline' )
-    return new CityDashboard.PolylineMarker( layer_params, attr, map, assoc_layer );
+    else if (attr.type === 'polyline')
+        return new CityDashboard.PolylineMarker(layer_params, attr, map, assoc_layer);
 
-  else if ( attr.type === 'simple' )
-    return new CityDashboard.SimpleMarker( layer_params, attr, map, assoc_layer );
+    else if (attr.type === 'simple')
+        return new CityDashboard.SimpleMarker(layer_params, attr, map, assoc_layer);
 
-};
+}
