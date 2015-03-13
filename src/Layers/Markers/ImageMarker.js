bddabeb84cc6bcd7212e824597f1851fea28718e
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-13 17:14:08 -0300

    Cleanup code of ImageMarker

--- src/Layers/Markers/ImageMarker.js
+++ src/Layers/Markers/ImageMarker.js
@@ -1,19 +1,23 @@
-CityDashboard.ImageMarker = function( layer_params, attr, map, assoc_layer ){
+CityDashboard.ImageMarker = function(layer_params, attr, map, assoc_layer) {
 
-  CityDashboard.Marker.call(this, layer_params, attr, map, assoc_layer);
+    CityDashboard.Marker.call(this, layer_params, attr, map, assoc_layer);
 
-  var myLatlng = new google.maps.LatLng( parseFloat(layer_params.lat), parseFloat(layer_params.lng) );
+    var myLatlng = new google.maps.LatLng(parseFloat(layer_params.lat), parseFloat(layer_params.lng));
 
-  var image = {url: attr.src || '../src/Layers/Markers/not_found.svg', scaledSize: new google.maps.Size(30,50)}
+    var image = {
+        // TODO: this relative reference breaks on minify
+        url: attr.src || '../src/Layers/Markers/not_found.svg',
+        scaledSize: new google.maps.Size(30, 50)
+    };
 
-  var imageMarker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      icon: image,
-      title: layer_params.landmark || ''
-  });
+    var imageMarker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        icon: image,
+        title: layer_params.landmark || ''
+    });
 
-  this.marker = imageMarker;
+    this.marker = imageMarker;
 
 };
 
@@ -21,40 +25,45 @@ CityDashboard.ImageMarker.prototype = Object.create(CityDashboard.Marker.prototy
 
 CityDashboard.ImageMarker.prototype = {
 
-  constructor: CityDashboard.ImageMarker,
+    constructor: CityDashboard.ImageMarker,
 
-  addEvents: function () {
-      
-    google.maps.event.addListener(this.marker, 'click', triggerEvent);
+    addEvents: function() {
 
-    var myself = this;
+        google.maps.event.addListener(this.marker, 'click', triggerEvent);
 
-    function triggerEvent() {
+        var myself = this;
 
-      $('#infoWindow').trigger('marker-pressed', {'id': myself.layer.id , 'value': myself.layer_params, 'attr': myself.attr});
+        function triggerEvent() {
 
-      for(var i = 0; i < myself.layer.markers.length; i++) {
-        var myMarker = myself.layer.markers[i].marker;
-        if (myMarker == this) {
-          myMarker.setAnimation(google.maps.Animation.BOUNCE);
+            $('#infoWindow').trigger('marker-pressed', {
+                'id': myself.layer.id,
+                'value': myself.layer_params,
+                'attr': myself.attr
+            });
+
+            for (var i = 0; i < myself.layer.markers.length; i++) {
+                var myMarker = myself.layer.markers[i].marker;
+                if (myMarker == this) {
+                    myMarker.setAnimation(google.maps.Animation.BOUNCE);
+                } else {
+                    myMarker.setAnimation(null);
+                }
+            }
+
         }
-        else {
-          myMarker.setAnimation(null);
+
+    },
+
+    triggerInitialEvent: function() {
+        $('#infoWindow').trigger('marker-pressed', {
+            'id': this.layer.id,
+            'value': this.layer_params,
+            'attr': this.attr
+        });
+        for (i = 0; i < this.layer.markers.length; i++) {
+            this.layer.markers[i].marker.setAnimation(null);
         }
-      }
-
+        this.marker.setAnimation(google.maps.Animation.BOUNCE);
     }
-
-  },
-
-  triggerInitialEvent: function() {
-    $('#infoWindow').trigger('marker-pressed', {'id': this.layer.id , 'value': this.layer_params, 'attr': this.attr});
-    for(i = 0; i < this.layer.markers.length; i++) {
-      this.layer.markers[i].marker.setAnimation(null);
-    }
-    this.marker.setAnimation(google.maps.Animation.BOUNCE);
-  }
 
 };
-
-
