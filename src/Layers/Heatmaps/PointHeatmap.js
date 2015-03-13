a514b1df6d2cc4f64bb9623eba7f6cba1b2a1a44
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-13 15:32:32 -0300

    Updated PointHeatmap to optionally use weighted maps

--- src/Layers/Heatmaps/PointHeatmap.js
+++ src/Layers/Heatmaps/PointHeatmap.js
@@ -1,21 +1,36 @@
-CityDashboard.PointHeatmap = function( layer_params, attr, map, assoc_layer ){
+CityDashboard.PointHeatmap = function(layer_params, attr, map, assoc_layer) {
 
-  var data = [];
+    var data = [];
 
-  var n = layer_params.points.length;
+    var len = layer_params.points.length;
 
-  for(var i = 0; i < n; i++){
-    data[i] = new google.maps.LatLng( parseFloat(layer_params.points[i].lat), parseFloat(layer_params.points[i].lng) );
-  }
+    if (layer_params.weight && layer_params.weight.length >= len) {
+        for (var i = 0; i < len; i++) {
+            data[i] = {
+                location: new google.maps.LatLng(
+                    parseFloat(layer_params.points[i].lat),
+                    parseFloat(layer_params.points[i].lng)
+                ),
+                weight: layer_params.weight[i]
+            }
+        }
+    } else {
+        for (var i = 0; i < len; i++) {
+            data[i] = new google.maps.LatLng(
+                parseFloat(layer_params.points[i].lat),
+                parseFloat(layer_params.points[i].lng)
+            );
+        }
+    }
 
-  var pointArray = new google.maps.MVCArray(data);
+    var pointArray = new google.maps.MVCArray(data);
 
-  this.heatmap = new google.maps.visualization.HeatmapLayer({
-    data: pointArray,
-    radius: attr.radius || 10
-  });
+    this.heatmap = new google.maps.visualization.HeatmapLayer({
+        data: pointArray,
+        radius: attr.radius || 10
+    });
 
-  this.heatmap.setMap(map);
+    this.heatmap.setMap(map);
 
 };
 
@@ -23,8 +38,6 @@ CityDashboard.PointHeatmap.prototype = Object.create(CityDashboard.Heatmap.proto
 
 CityDashboard.PointHeatmap.prototype = {
 
-  constructor: CityDashboard.PointHeatmap,
+    constructor: CityDashboard.PointHeatmap,
 
 };
-
-
