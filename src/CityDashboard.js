b22e6248e4586b28967c4e329a3804309d18e2d0
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-14 11:04:59 -0300

    Convert CityDasbhboard to the module pattern

--- src/CityDashboard.js
+++ src/CityDashboard.js
@@ -1,17 +1,31 @@
-var CityDashboard = {
-  mainContainerID: '#city-dashboard',
-  mapWindowID: '#mapWindow',
-  infoWindowID: '#infoWindow',
-  filterBarID: '#filterBar',
-  getData: function ( url, callback, props ) {
-    if ( url.charAt(0) !== '#' ){
-      $.getJSON(url,function( json ){
-        props.data = json;
-        return callback( props );
-      });
-    }
-    else {
-      return callback( props );
-    }
-  }
-};
+var CityDashboard = (function($) {
+    "use strict";
+
+    return {
+        mainContainerID: '#city-dashboard',
+        mapWindowID: '#mapWindow',
+        infoWindowID: '#infoWindow',
+        filterBarID: '#filterBar',
+
+        /**
+         * Get the data for the dashboard
+         *
+         * @param {String} uri uri or id for the data source
+         * @param {Callback} callback function to pass the data to
+         * @param {Object} mapping of properties to pass to the callback
+         * @return {Object} result for the callback
+         */
+        getData: function(uri, callback, props) {
+            if (uri.charAt(0) !== '#') {
+                $.getJSON(uri, function(json) {
+                    props.data = json;
+
+                    // TODO: The return value of callback is never used
+                    return callback(props);
+                });
+            } else {
+                return callback(props);
+            }
+        }
+    };
+})(jQuery);
