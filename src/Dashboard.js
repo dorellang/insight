75375ce1014fa902c5280c432573e4df4ebb9144
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-14 18:01:29 -0300

    Cleanup Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,89 +1,89 @@
-CityDashboard.Dashboard = function ( parameters ) {
+CityDashboard.Dashboard = function(parameters) {
 
-  this.layers = [];
+    this.layers = [];
 
-  if (!parameters.anchor)
-    throw new Error( 'Anchor ID is required.');
+    if (!parameters.anchor)
+        throw new Error('Anchor ID is required.');
 
-  this.anchor = parameters.anchor;
+    this.anchor = parameters.anchor;
 
-  this.layout = 'layout-' + ( parameters.layout || 'none' );
+    this.layout = 'layout-' + (parameters.layout || 'none');
 
-  // placing
+    // placing
 
-  var container = $( '<div>' )
-  .setID( CityDashboard['mainContainerID'] ).addClass('mainDashboard')
-  .addClass( this.layout );
+    var container = $('<div>')
+        .setID(CityDashboard['mainContainerID']).addClass('mainDashboard')
+        .addClass(this.layout);
 
-  var infoDiv;
-  if ( this.layout !== 'layout-none' ) {
-    infoDiv = $('<div>')
-    .setID( CityDashboard['infoWindowID'] ).addClass('infoWindow');
+    var infoDiv;
+    if (this.layout !== 'layout-none') {
+        infoDiv = $('<div>')
+            .setID(CityDashboard['infoWindowID']).addClass('infoWindow');
 
-    var resizeOrientation;
-    if ( this.layout === 'layout-left') 
-      resizeOrientation = 'e';
-    else if ( this.layout === 'layout-right') 
-      resizeOrientation = 'w';
-    // else if ( this.layout === 'layout-top') 
-    //   resizeOrientation = 's';
-    // else if ( this.layout === 'layout-bottom') 
-    //   resizeOrientation = 'n';
+        var resizeOrientation;
+        if (this.layout === 'layout-left')
+            resizeOrientation = 'e';
+        else if (this.layout === 'layout-right')
+            resizeOrientation = 'w';
+        // else if ( this.layout === 'layout-top')
+        //   resizeOrientation = 's';
+        // else if ( this.layout === 'layout-bottom')
+        //   resizeOrientation = 'n';
 
-    infoDiv.resizable( resizeOrientation );
+        infoDiv.resizable(resizeOrientation);
 
-    container.append( infoDiv );
-  }
+        container.append(infoDiv);
+    }
 
-  var mapDiv = $('<div>')
-  .setID( CityDashboard['mapWindowID'] ).addClass('mapWindow');
-  container.append( mapDiv );
+    var mapDiv = $('<div>')
+        .setID(CityDashboard['mapWindowID']).addClass('mapWindow');
+    container.append(mapDiv);
 
-  $( this.anchor ).append( container );
+    $(this.anchor).append(container);
 
-  this.filters = new CityDashboard.FilterBar(parameters['filter-number'] || 0);
-
-  var layers = this.layers;
-
-  container.on('filterChanged',function (e, fun) {
-    var f = fun()
-    for (var i = layers.length - 1; i >= 0; i--) {
-      layers[i].filter(f);
-    };
-  });
-
-}
-
-CityDashboard.Dashboard.prototype = {
-
-  constructor: CityDashboard.Dashboard,
-
-  addLayer: function ( parameters ) {
+    this.filters = new CityDashboard.FilterBar(parameters['filter-number'] || 0);
 
     var layers = this.layers;
 
-    var callback = function (pr) {
-      layers[layers.length] = LayerSelector( pr,
-                                  $(CityDashboard['mainContainerID'])[0].data );
-    }// gmap: $(CityDashboard['mainContainerID'])[0].data
+    container.on('filterChanged', function(e, fun) {
+        var f = fun();
+        for (var i = layers.length - 1; i >= 0; i--) {
+            layers[i].filter(f);
+        }
+    });
 
-    CityDashboard.getData(parameters['data-source'],callback,parameters);
+};
 
-    return this;
+CityDashboard.Dashboard.prototype = {
 
-  },
+    constructor: CityDashboard.Dashboard,
 
-  addFilter: function ( filters ) {
+    addLayer: function(parameters) {
 
-    this.filters.addFilter( filters );
+        var layers = this.layers;
 
-    return this;
-  },
+        var callback = function(pr) {
+                layers[layers.length] = LayerSelector(pr,
+                    $(CityDashboard['mainContainerID'])[0].data);
+            } // gmap: $(CityDashboard['mainContainerID'])[0].data
 
-  clear: function () {
-    for (var i = 0; i < this.layers.length; i++) {
-      this.layers[i].clear();
-    };
-    this.layers = [];
-  }
+        CityDashboard.getData(parameters['data-source'], callback, parameters);
+
+        return this;
+
+    },
+
+    addFilter: function(filters) {
+
+        this.filters.addFilter(filters);
+
+        return this;
+    },
+
+    clear: function() {
+        for (var i = 0; i < this.layers.length; i++) {
+            this.layers[i].clear();
+        };
+        this.layers = [];
+    }
 };
