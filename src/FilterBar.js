3f59197fd7826823a799191c53cdee175b810a29
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-16 14:59:38 -0300

    Simplified FilterBar

--- src/FilterBar.js
+++ src/FilterBar.js
@@ -6,23 +6,53 @@ var CityDashboard = CityDashboard || {};
 CityDashboard.FilterBar = (function(CityDashboard, google, $) {
     "use strict";
 
-    var FilterBar = function(filterNumber) {
-        this.filters = {
-            none: function(data) {
-                return true;
-            }
-        };
-        this.filterNumber = filterNumber;
+    return function() {
+        // Bar element
+        var bar = $('<div>').setID(CityDashboard.id('filters')).addClass('filterBar');
 
-        this.bar = $('<div>').setID(CityDashboard.id('filters')).addClass('filterBar');
+        // List of filters
+        var filters = [];
+
+        /**
+         * Compose the filters selected by the user
+         */
+        function composeFilters() {
+            var callbacks = [];
+            for (var i = 0; i < filters.length; i++) {
+                if (filters[i].element.prop('selectedIndex') > 0) {
+                    callbacks.push(filters[i].options[filters[i].element.prop('selectedIndex') - 1].filter);
+                }
+            }
+
+            return function(data) {
+                for (var i = 0; i < callbacks.length; i++) {
+                    if (!callbacks[i](data)) return false;
+                }
+                return true;
+            };
+        }
+
+        /**
+         * Callback to use on filter change
+         */
+        function onFilterChange() {
+            CityDashboard.container('main').trigger('filterChanged', function() {
+                return composeFilters();
+            });
+        }
 
         /* Google maps geocoder and search bar*/
         var geocoder = new google.maps.Geocoder();
 
+        // Create the search box
+        var search = bar.append($('<input>')
+            .addClass('search')
+            .attr('type', 'search')
+            .attr('placeholder', 'Enter location'));
+
         var geocode = function() {
             var map = CityDashboard.container('main')[0].data;
-            console.log(CityDashboard.container('main'));
-            var address = $(CityDashboard.id('filters') + ' > .search').val();
+            var address = search.val();
             geocoder.geocode({
                 'address': address
             }, function(results, status) {
@@ -31,94 +61,62 @@ CityDashboard.FilterBar = (function(CityDashboard, google, $) {
                     // map.setZoom(12);
                     map.fitBounds(results[0].geometry.bounds);
                 } else {
-                    $(CityDashboard.id('filters') + ' > .search').val('not found: ' + address);
+                    // TODO: this message should go in a status bar
+                    search.val('not found: ' + address);
                 }
             });
         };
 
-        CityDashboard.container('main').append(this.bar);
+        search.on('change', geocode);
 
-        this.bar.append($('<input>')
-            .addClass('search')
-            .attr('type', 'search')
-            .attr('placeholder', 'Enter location')
-            .on('change', geocode));
+        // Append the filter bar to the dashboard
+        CityDashboard.container('main').append(bar);
 
-        var self = this;
-        this.select = [];
+        return {
+            /**
+             * Add a new filter to the filter bar, displayed as a <select> object in the UI, it returns the jquery element
+             * of the filter for further customizations
+             *
+             * Example:
+             * ```
+             * myDashboard.addFilter({
+             *  description: 'Geographic Location', // the empty string is used if not provided
+             *  options: [
+             *      {name: 'More than 20s', filter: function (data) {return data.seconds > 20;}},
+             *      {name: 'Over Equator', filter: function (data) {return data.lat > 0;}},
+             *      {name: 'By Type: a,f,g,e,t,h', filter: function (data) {return "afgeth".indexOf(data['event type'])> 0;}}
+             *  ]
+             * });
+             * ```
+             *
+             * @param (Object) filter configuration for the filter
+             * @return (jQuery) reference to the added element for further customization
+             */
+            addFilter: function(filter) {
+                var select = $('<select>');
 
-        var onChange = function() {
-            CityDashboard.container('main').trigger('filterChanged', function() {
-                return self.composeFilters();
-            });
+                var description = filter.description || '';
+                var option = $('<option>').text(description);
+                select.append(option);
 
+                var i;
+                for (i = 0; i < filter.options.length; i++) {
+                    option = $('<option>').text(filter.options[i].name);
+                    select.append(option);
+                }
+
+                select.on('change', onFilterChange);
+
+                // Add the selector to the filter bar
+                bar.append(select);
+
+                filter.element = select;
+
+                // Add the filter to the filter list
+                filters.push(filter);
+
+                return select;
+            },
         };
-
-        for (var i = 0; i < this.filterNumber; i++) {
-            this.select[i] = $('<select>').on('change', onChange);
-            this.bar.append(this.select[i]);
-        }
-        this.placeFilters();
-
     };
-
-    FilterBar.prototype = {
-        constructor: CityDashboard.FilterBar,
-
-        /**
-         * TODO: missing documentation
-         */
-        addFilter: function(filters) {
-            for (var key in filters) {
-                this.filters[key] = filters[key];
-            }
-            for (var i = this.select.length - 1; i >= 0; i--) {
-                this.select[i].empty();
-            }
-            this.placeFilters();
-        },
-
-        /**
-         * TODO: missing documentation
-         */
-        placeFilters: function() {
-            var self = this;
-
-            var getFilter = function(key) {
-                return function() {
-                    return self.filters[key];
-                };
-            };
-
-            for (var j = 0; j < this.filterNumber; j++) {
-                for (var key in this.filters) {
-                    var option = $('<option>');
-
-                    option.val(getFilter(key));
-                    option.text(key);
-                    this.select[j].append(option);
-                }
-            }
-        },
-
-        /**
-         * TODO: missing documentation
-         */
-        composeFilters: function() {
-            var l = [];
-            for (var i = this.select.length - 1; i >= 0; i--) {
-                // TODO: why this definition? Using eval is bad practice
-                l[l.length] = Function('return ' + this.select[i].val())();
-            }
-            return function(data) {
-                for (var i = l.length - 1; i >= 0; i--) {
-                    if (!l[i](data))
-                        return false;
-                }
-                return true;
-            };
-        }
-    };
-
-    return FilterBar;
 })(CityDashboard, google, jQuery);
