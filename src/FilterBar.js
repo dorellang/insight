11a57ce68cb2b5daf9aba749d7cb95f275b8acac
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-16 11:57:03 -0300

    Move FilterBar to module pattern

--- src/FilterBar.js
+++ src/FilterBar.js
@@ -1,113 +1,124 @@
-CityDashboard.FilterBar = function(filterNumber) {
-    this.filters = {
-        none: function(data) {
-            return true;
-        }
-    };
-    this.filterNumber = filterNumber;
+var CityDashboard = CityDashboard || {};
 
-    this.bar = $('<div>').setID(CityDashboard.id('filters')).addClass('filterBar');
+/**
+ * Defines the behavior of the filter bar of the dashboard
+ */
+CityDashboard.FilterBar = (function(CityDashboard, google, $) {
+    "use strict";
 
-    /* Google maps geocoder and search bar*/
-
-    var geocoder = new google.maps.Geocoder();
-
-    var geocode = function() {
-        var map = $(CityDashboard.id('main'))[0].data;
-        var address = $(CityDashboard.id('filters') + ' > .search').val();
-        geocoder.geocode({
-            'address': address
-        }, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                map.setCenter(results[0].geometry.location);
-                // map.setZoom(12);
-                map.fitBounds(results[0].geometry.bounds);
-            } else {
-                $(CityDashboard.id('filters') + ' > .search').val('not found: ' + address);
+    var FilterBar = function(filterNumber) {
+        this.filters = {
+            none: function(data) {
+                return true;
             }
-        });
-    };
+        };
+        this.filterNumber = filterNumber;
 
-    CityDashboard.container('main').append(this.bar);
+        this.bar = $('<div>').setID(CityDashboard.id('filters')).addClass('filterBar');
 
-    this.bar.append($('<input>')
-        .addClass('search')
-        .attr('type', 'search')
-        .attr('placeholder', 'Enter location')
-        .on('change', geocode));
+        /* Google maps geocoder and search bar*/
+        var geocoder = new google.maps.Geocoder();
 
-    var _this = this;
-    this.select = [];
+        var geocode = function() {
+            var map = CityDashboard.container('main')[0].data;
+            console.log(CityDashboard.container('main'));
+            var address = $(CityDashboard.id('filters') + ' > .search').val();
+            geocoder.geocode({
+                'address': address
+            }, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    map.setCenter(results[0].geometry.location);
+                    // map.setZoom(12);
+                    map.fitBounds(results[0].geometry.bounds);
+                } else {
+                    $(CityDashboard.id('filters') + ' > .search').val('not found: ' + address);
+                }
+            });
+        };
 
-    var onChange = function() {
-        CityDashboard.container('main').trigger('filterChanged', function() {
-            return _this.composeFilters();
-        });
+        CityDashboard.container('main').append(this.bar);
 
-    };
+        this.bar.append($('<input>')
+            .addClass('search')
+            .attr('type', 'search')
+            .attr('placeholder', 'Enter location')
+            .on('change', geocode));
 
-    for (var i = 0; i < this.filterNumber; i++) {
-        this.select[i] = $('<select>').on('change', onChange);
-        this.bar.append(this.select[i]);
-    }
-    this.placeFilters();
+        var self = this;
+        this.select = [];
 
-};
+        var onChange = function() {
+            CityDashboard.container('main').trigger('filterChanged', function() {
+                return self.composeFilters();
+            });
 
-CityDashboard.FilterBar.prototype = {
-    constructor: CityDashboard.FilterBar,
+        };
 
-    /**
-     * TODO: missing documentation
-     */
-    addFilter: function(filters) {
-        for (var key in filters) {
-            this.filters[key] = filters[key];
-        }
-        for (var i = this.select.length - 1; i >= 0; i--) {
-            this.select[i].empty();
+        for (var i = 0; i < this.filterNumber; i++) {
+            this.select[i] = $('<select>').on('change', onChange);
+            this.bar.append(this.select[i]);
         }
         this.placeFilters();
-    },
 
-    /**
-     * TODO: missing documentation
-     */
-    placeFilters: function() {
-        var _this = this;
+    };
 
-        var getFilter = function(key) {
-            return function() {
-                return _this.filters[key];
+    FilterBar.prototype = {
+        constructor: CityDashboard.FilterBar,
+
+        /**
+         * TODO: missing documentation
+         */
+        addFilter: function(filters) {
+            for (var key in filters) {
+                this.filters[key] = filters[key];
+            }
+            for (var i = this.select.length - 1; i >= 0; i--) {
+                this.select[i].empty();
+            }
+            this.placeFilters();
+        },
+
+        /**
+         * TODO: missing documentation
+         */
+        placeFilters: function() {
+            var self = this;
+
+            var getFilter = function(key) {
+                return function() {
+                    return self.filters[key];
+                };
             };
-        };
 
-        for (var j = 0; j < this.filterNumber; j++) {
-            for (var key in this.filters) {
-                var option = $('<option>');
+            for (var j = 0; j < this.filterNumber; j++) {
+                for (var key in this.filters) {
+                    var option = $('<option>');
 
-                option.val(getFilter(key));
-                option.text(key);
-                this.select[j].append(option);
+                    option.val(getFilter(key));
+                    option.text(key);
+                    this.select[j].append(option);
+                }
             }
-        }
-    },
+        },
 
-    /**
-     * TODO: missing documentation
-     */
-    composeFilters: function() {
-        var l = [];
-        for (var i = this.select.length - 1; i >= 0; i--) {
-            // TODO: why this definition? Using eval is bad practice
-            l[l.length] = Function('return ' + this.select[i].val())();
-        }
-        return function(data) {
-            for (var i = l.length - 1; i >= 0; i--) {
-                if (!l[i](data))
-                    return false;
+        /**
+         * TODO: missing documentation
+         */
+        composeFilters: function() {
+            var l = [];
+            for (var i = this.select.length - 1; i >= 0; i--) {
+                // TODO: why this definition? Using eval is bad practice
+                l[l.length] = Function('return ' + this.select[i].val())();
             }
-            return true;
-        };
-    }
-};
+            return function(data) {
+                for (var i = l.length - 1; i >= 0; i--) {
+                    if (!l[i](data))
+                        return false;
+                }
+                return true;
+            };
+        }
+    };
+
+    return FilterBar;
+})(CityDashboard, google, jQuery);
