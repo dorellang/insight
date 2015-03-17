612d4a474d1f6f751c4f17550cda7111354d6bfb
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-17 12:13:00 -0300

    Maps/GoogleMap now inherits from base Map

--- src/Maps/GoogleMap.js
+++ src/Maps/GoogleMap.js
@@ -1,28 +1,87 @@
-CityDashboard.GoogleMap = function ( parameters ) {
-	
-	// this.observers = [];
+var CityDashboard = CityDashboard || {};
 
-  this.center = {
-    'lat': parameters.lat || 0,
-    'lng': parameters.lng || 0
-  };
+CityDashboard.GoogleMap = (function(CityDashboard, $) {
+    return function(options) {
+        // Initialize parent
+        var map = CityDashboard.Map(options);
 
-  this.zoom = parameters.zoom || 12;
+        var googlemap = new google.maps.Map(map.container()[0], {
+            zoom: map.zoom(),
+            center: new google.maps.LatLng(map.lat(), map.lng()),
+            disableDefaultUI: true
+        });
 
-  var mapContainer = $(CityDashboard['mapWindowID'])[0];
+		// Store the google map in the citydashboard
+		// TODO: this is very poor practice
+		CityDashboard.container('main')[0].data = googlemap;
 
-  this.googlemap = new google.maps.Map( mapContainer, {
-    zoom: this.zoom,
-    center: new google.maps.LatLng(this.center.lat, this.center.lng),
-    disableDefaultUI: true
-  });
+		// Store the parent function
+		var zoom = map.zoom;
 
-  $(CityDashboard['mainContainerID'])[0].data = this.googlemap;  
-  
-};
+		// Listen for zoom changes
+		google.maps.event.addListener(googlemap, 'zoom_changed', function() {
+			// Update the map zoom
+		    zoom(googlemap.getZoom());
+		});
 
-CityDashboard.GoogleMap.prototype = {
+		/**
+		 * Set/get the zoom level for the map
+		 *
+		 * Overrides the functionality of CityDashboard.Map.zoom() by modifying
+		 * the underlying google map zoom level as well
+		 *
+		 * @param (int) level optional zoom level
+		 * @return (int) zoom level for the map
+		 */
+		map.zoom = function(level) {
+			// Call parent zoom
+			level = zoom(level);
 
-  constructor: CityDashboard.Map
+			// Update the google map
+			googlemap.setZoom(level);
 
-};
+			return level;
+		};
+
+		// Store the parent function
+		var center = map.center;
+
+		// Listen for center changes
+		google.maps.event.addListener(googlemap, 'center_changed', function() {
+		    var c = googlemap.getCenter();
+
+			// Update the center for the local object
+			center(c.lat(), c.lng());
+		});
+
+		/**
+		* Set/get the map center.
+		*
+		* Overrides the functionality of CityDashboard.Map.center() by modifying
+		* the underlying google map center as well
+		*
+		* @param (float) lat optional latitude for the map
+		* @param (float) lng optional longitude for the map
+		* @returns (Object) {lat: <center latitude>, lng: <center longitude>}
+		*/
+		map.center = function(lat, lng) {
+			var c = center(lat, lng);
+
+			// Update the map
+			googlemap.setCenter({'lat': c.lat, 'lng': c.lng});
+
+			return c;
+		};
+
+		/**
+		 * Get the underlying google map object for further modification
+		 *
+		 * @returns (google.maps.Map) underlying map object
+		 */
+		map.googlemap = function() {
+			return googlemap;
+		};
+
+		return map;
+    };
+})(CityDashboard, jQuery);
