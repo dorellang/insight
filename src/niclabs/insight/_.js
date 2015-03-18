b20162198947c0ea8f6f99d7bda18c7f28b4f11c
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-18 17:24:01 -0300

    Setup namespace files

---
+++ src/niclabs/insight/_.js
@@ -0,0 +1,11 @@
+var niclabs = niclabs || {};
+
+/**
+ * Insight is a generic web dashboard for smart city projects.
+ *
+ * It allows to combine different chart and visualization tools to better
+ * understand what is going on in the city
+ *
+ * @namespace
+ */
+niclabs.insight = {};
