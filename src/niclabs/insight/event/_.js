35710b139b1c9ec5b4c098b0dc2a1468a6404862
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-18 18:08:06 -0300

    Created namespace niclabs.insight.event for dashboard events handling

---
+++ src/niclabs/insight/event/_.js
@@ -0,0 +1,74 @@
+/**
+ * Register/trigger events for the dashboard
+ *
+ * @namespace
+ */
+niclabs.insight.event = (function() {
+    "use strict";
+
+    var events = {};
+
+    /**
+     * Find the event in the event list, return -1 if not found
+     */
+    function indexOf(event, listener) {
+        if (event in events) {
+            for (var i = 0; i < events[event].length; i++) {
+                if (events[event][i] === listener) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Insight event listener
+     *
+     * @callback niclabs.insight.event~listener
+     * @param {Object} data - data for the callback function, dependant on the event
+     */
+
+    return {
+        /**
+         * Listen for an event
+         *
+         * @memberof niclabs.insight.event
+         * @param {string} event - event type
+         * @param {niclabs.insight.event~listener} listener - callback to process the event
+         * @param {boolean} subscribe - true to subscribe to listen / false to stop listening
+         */
+        listen: function(event, listener, subscribe) {
+            var index = indexOf(event, listener);
+
+            if (subscribe && index < 0) {
+                if (!(event in events)) {
+                    events[event] = [];
+                }
+
+                // Add the new listener
+                events[event].push(listener);
+            }
+            else if (!subscribe && index >= 0) {
+                // Remove the event
+                events[event].splice(index, 1);
+            }
+        },
+
+        /**
+         * Trigger an event
+         *
+         * @memberof niclabs.insight.event
+         * @param {string} event - event type
+         * @param {Object=} data - data to pass to the callback
+         */
+        trigger: function(event, data) {
+            if (event in events) {
+                for (var i = 0; i < events[event].length; i++) {
+                    // Notify the listeners
+                    events[event][i](data);
+                }
+            }
+        }
+    };
+})();
