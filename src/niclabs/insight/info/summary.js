4e736ecf3dca834c0d04bb1bbe76a69a84c91e31
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-19 17:19:59 -0300

    Update documentation of Summary

--- src/niclabs/insight/info/summary.js
+++ src/niclabs/insight/info/summary.js
@@ -1,4 +1,14 @@
 niclabs.insight.info.Summary = (function($) {
+    /**
+     * Construct a new summary information block
+     * TODO: describe what is a summary information block
+     *
+     * @class niclabs.insight.info.Summary
+     * @augments niclabs.insight.info.Block
+     * @inheritdoc
+     * @param {niclabs.insight.Dashboard} dashboard - parent dashboard for the block
+     * @param {Object} options - see {@link niclabs.insight.info.Block} constructor
+     */
     var constructor = function(dashboard, options) {
         var self = niclabs.insight.info.Block(dashboard, options);
 
@@ -7,6 +17,9 @@ niclabs.insight.info.Summary = (function($) {
         // Store the refresh method of the parent
         var refresh = self.refresh;
 
+        /**
+         * Override the parent refresh
+         */
         self.refresh = function() {
             // Call the parent refresh
             refresh();
@@ -20,4 +33,6 @@ niclabs.insight.info.Summary = (function($) {
 
     // Register the handler
     niclabs.insight.handler('summary-block', 'info-block', constructor);
+
+    return constructor;
 })(jQuery);
