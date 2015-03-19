63a2f4c1675dca576b2dd1ca4e543d66964e536c
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-19 15:40:25 -0300

    Split listen() method of event into on()/off()

--- src/niclabs/insight/event/_.js
+++ src/niclabs/insight/event/_.js
@@ -1,5 +1,19 @@
 /**
- * Register/trigger events for the dashboard
+ * Very basic event manager for the dashboard
+ *
+ * @example
+ * ```javascript
+ * // Subscribe to the event
+ * var eventId = niclabs.insight.event.on('hello', function(who) {
+ *      alert("HELLO "+who+"!!!");
+ * });
+ *
+ * // Trigger the event
+ * niclabs.insight.event.trigger('hello', "John"); // Shows alert 'HELLO John!!!'
+ *
+ * // Unsubscribe
+ * niclabs.insight.event.off('hello', eventId);
+ * ```
  *
  * @namespace
  */
@@ -31,29 +45,45 @@ niclabs.insight.event = (function() {
 
     return {
         /**
-         * Listen for an event
+         * Listen for an event. A listener callback can only be assigned once for an event
          *
          * @memberof niclabs.insight.event
          * @param {string} event - event type
          * @param {niclabs.insight.event~listener} listener - callback to process the event
-         * @param {boolean} [subscribe=true] - true to subscribe to listen / false to stop listening
+         * @returns {number} id of the listener
          */
-        listen: function(event, listener, subscribe) {
+        on: function(event, listener) {
             var index = indexOf(event, listener);
-            subscribe = typeof subscribe === 'undefined' ? true : subscribe;
 
-            if (subscribe && index < 0) {
+            if (index < 0) {
                 if (!('event' in events)) {
                     events[event] = [];
                 }
 
                 // Add the new listener
-                events[event].push(listener);
+                return events[event].push(listener) - 1;
             }
-            else if (!subscribe && index >= 0) {
+            return index;
+        },
+
+        /**
+         * Stop listening for an event.
+         *
+         * @memberof niclabs.insight.event
+         * @param {string} event - event type
+         * @param {niclabs.insight.event~listener|number} listener - callback to remove or id of the listener provided by {@link niclabs.insight.event.on()}
+         * @returns {boolean} true if the listener was found and was succesfully removed
+         */
+        off: function(event, listener) {
+            var index = typeof listener === 'number' ? listener : indexOf(event, listener);
+
+            if (index >= 0) {
                 // Remove the event
                 events[event].splice(index, 1);
+
+                return true;
             }
+            return false;
         },
 
         /**
