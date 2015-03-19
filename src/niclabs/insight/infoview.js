2ff162942fec0eb88524e0450f908735c5232fbe
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-19 13:21:11 -0300

    Initial cleanup of info view for refactoring

--- src/niclabs/insight/infoview.js
+++ src/niclabs/insight/infoview.js
@@ -1,113 +1,154 @@
-var CityDashboard = CityDashboard || {};
-
-CityDashboard.InfoWindow = (function(CityDashboard, $) {
+niclabs.insight.InfoView = (function($) {
     "use strict";
 
-    return function(vizPropList) {
+    /**
+     * Construct the dashboard information view
+     *
+     * The information view is composed of information blocks to show
+     * different aspects of the data shown in the map or
+     * about the visualization in general
+     *
+     * @class niclabs.insight.InfoView
+     * @param {niclabs.insight.Dashboard} dashboard - dashboard to assign this info view to
+     * @param {Object} options - list of configuration options for the information view
+     */
+    var constructor = function(dashboard, options) {
         // Default visualization property list
-        vizPropList = vizPropList || [];
+        options = options || {};
 
-        var visualizations = {};
-        var dataSourceTable = {};
+        var infoViewId = "#insight-infoView";
 
-        /**
-         * Create a visualization according to the type provided in
-         * props.visualization
-         *
-         * TODO: there must be a better way to do this
-         *
-         * @param (Object) props properties for the visualization
-         * @return (Object) visualization after data is loaded
-         */
-        function createVisualization(props) {
-            var type = props.visualization;
+        // Create the info view
+        var container;
+        if (dashboard.config('layout') !== 'none') {
+            container = $('<div>')
+                .setID(infoViewId)
+                .addClass('infoWindow');
 
-            var callback = function(pr) {
-                var viz;
-
-                if (!type)
-                    return;
-
-                else if (type === 'summary-viz')
-                    viz = new CityDashboard.SummaryVisualization(pr);
-                else if (type === 'linechart-viz')
-                    // TODO: check that the library is loaded first
-                    viz = new CityDashboard.ChartistVisualization(pr, Chartist.Line);
-                else if (type === 'barchart-viz')
-                    viz = new CityDashboard.ChartistVisualization(pr, Chartist.Bar);
-                else if (type === 'piechart-viz')
-                    viz = new CityDashboard.ChartistVisualization(pr, Chartist.Pie);
-                else if (type === 'd3-viz')
-                    viz = new CityDashboard.D3Visualization(pr);
-                else if (type === 'general-viz')
-                    viz = new CityDashboard.GeneralVisualization(pr);
-
-                visualizations[viz.id] = viz;
-                dataSourceTable[viz.data_source] = dataSourceTable[viz.data_source] || [];
-                dataSourceTable[viz.data_source].push(viz);
-
-                return viz;
-            };
-
-            return CityDashboard.getData(props['data-source'], callback, props);
+            var resizeOrientation;
+            if (dashboard.config('layout') === 'left') {
+                // TODO: move filter bar
+                resizeOrientation = 'e';
+            }
+            else if (dashboard.config('layout') === 'right') {
+                resizeOrientation = 'w';
+            }
+            container.resizable(resizeOrientation);
+            $(dashboard.element()).append(container);
         }
 
-        // Create the visualizations in the property list
-        for (var i = 0; i < vizPropList.length; i++) {
-            createVisualization(vizPropList[i]);
-        }
+        // var visualizations = {};
+        // var dataSourceTable = {};
 
-        // Get the window element
-        var infoWindow = CityDashboard.container('info');
+        // /**
+        //  * Create a visualization according to the type provided in
+        //  * props.visualization
+        //  *
+        //  * TODO: there must be a better way to do this
+        //  *
+        //  * @param (Object) props properties for the visualization
+        //  * @return (Object) visualization after data is loaded
+        //  */
+        // function createVisualization(props) {
+        //     var type = props.visualization;
+        //
+        //     var callback = function(pr) {
+        //         var viz;
+        //
+        //         if (!type)
+        //             return;
+        //
+        //         else if (type === 'summary-viz')
+        //             viz = new CityDashboard.SummaryVisualization(pr);
+        //         else if (type === 'linechart-viz')
+        //             // TODO: check that the library is loaded first
+        //             viz = new CityDashboard.ChartistVisualization(pr, Chartist.Line);
+        //         else if (type === 'barchart-viz')
+        //             viz = new CityDashboard.ChartistVisualization(pr, Chartist.Bar);
+        //         else if (type === 'piechart-viz')
+        //             viz = new CityDashboard.ChartistVisualization(pr, Chartist.Pie);
+        //         else if (type === 'd3-viz')
+        //             viz = new CityDashboard.D3Visualization(pr);
+        //         else if (type === 'general-viz')
+        //             viz = new CityDashboard.GeneralVisualization(pr);
+        //
+        //         visualizations[viz.id] = viz;
+        //         dataSourceTable[viz.data_source] = dataSourceTable[viz.data_source] || [];
+        //         dataSourceTable[viz.data_source].push(viz);
+        //
+        //         return viz;
+        //     };
+        //
+        //     return CityDashboard.getData(props['data-source'], callback, props);
+        // }
 
-        var handler = function(event, arg) {
-            infoWindow.off('marker-pressed');
+        // // Create the visualizations in the property list
+        // for (var i = 0; i < options.length; i++) {
+        //     createVisualization(options[i]);
+        // }
 
-            if (arg.attr.id && !(arg.attr.id in visualizations)) {
-                var config = $.extend({}, arg.attr);
-                config.data = arg.value;
-                config['data-source'] = arg.id;
-                // {
-                //   'visualization': arg['attr']['visualization'],
-                //   'id': arg['attr']['id'],
-                //   'data-source': arg['id'],
-                //   'data': arg.value,
-                //   'preprocess': arg['attr']['preprocess'],
-                //   'title': arg['attr']['title'],
-                //   'properties': arg['attr']['properties'],
-                //   'labels': arg['attr']['labels'],
-                //   'checkbox': arg['attr']['checkbox'],
-                //   'checkbox-handler': arg['attr']['checkbox-handler'],
-                //   'viz': arg['attr']['viz']
-                // };
+        // // Get the window element
+        // var infoWindow = CityDashboard.container('info');
 
-                createVisualization(config);
+        // var handler = function(event, arg) {
+        //     infoWindow.off('marker-pressed');
+        //
+        //     if (arg.attr.id && !(arg.attr.id in visualizations)) {
+        //         var config = $.extend({}, arg.attr);
+        //         config.data = arg.value;
+        //         config['data-source'] = arg.id;
+        //         // {
+        //         //   'visualization': arg['attr']['visualization'],
+        //         //   'id': arg['attr']['id'],
+        //         //   'data-source': arg['id'],
+        //         //   'data': arg.value,
+        //         //   'preprocess': arg['attr']['preprocess'],
+        //         //   'title': arg['attr']['title'],
+        //         //   'properties': arg['attr']['properties'],
+        //         //   'labels': arg['attr']['labels'],
+        //         //   'checkbox': arg['attr']['checkbox'],
+        //         //   'checkbox-handler': arg['attr']['checkbox-handler'],
+        //         //   'viz': arg['attr']['viz']
+        //         // };
+        //
+        //         createVisualization(config);
+        //     }
+        //
+        //     var vizs = dataSourceTable[arg.id] || [];
+        //     for (var i = vizs.length - 1; i >= 0; i--) {
+        //         vizs[i].setData(arg.value);
+        //         vizs[i].refresh();
+        //     }
+        //
+        //     infoWindow.on('marker-pressed', handler);
+        // };
+
+        // // Add the handler
+        // infoWindow.on('marker-pressed', handler);
+        //
+        // // Add a resize handler
+        // infoWindow.on('resize', function(e) {
+        //     for (var key in visualizations) {
+        //         visualizations[key].refresh();
+        //     }
+        // });
+        //
+        // // Perform cleanup on visualization removal
+        // infoWindow.on('remove-viz', function(e, arg) {
+        //     delete visualizations[arg.id];
+        //     var index = $.inArray(dataSourceTable[arg['data-source']], arg.id);
+        //     dataSourceTable[arg['data-source']].splice(index, 1);
+        // });
+
+        var self = {
+            element: function() {
+                return $(infoViewId)[0];
             }
-
-            var vizs = dataSourceTable[arg.id] || [];
-            for (var i = vizs.length - 1; i >= 0; i--) {
-                vizs[i].setData(arg.value);
-                vizs[i].refresh();
-            }
-
-            infoWindow.on('marker-pressed', handler);
         };
-
-        // Add the handler
-        infoWindow.on('marker-pressed', handler);
-
-        // Add a resize handler
-        infoWindow.on('resize', function(e) {
-            for (var key in visualizations) {
-                visualizations[key].refresh();
-            }
-        });
-
-        // Perform cleanup on visualization removal
-        infoWindow.on('remove-viz', function(e, arg) {
-            delete visualizations[arg.id];
-            var index = $.inArray(dataSourceTable[arg['data-source']], arg.id);
-            dataSourceTable[arg['data-source']].splice(index, 1);
-        });
     };
-})(CityDashboard, jQuery);
+
+    // Register the info view constructor
+    niclabs.insight.handler('basic-info-view', 'info-view', constructor);
+
+    return constructor;
+})(jQuery);
