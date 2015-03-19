5b787f1eec52cf7a00ce12a8720f7c4ad68901a7
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-19 15:40:25 -0300

    Split listen() method of event into on()/off()

--- test/js/tests.js
+++ test/js/tests.js
@@ -6,14 +6,24 @@ QUnit.test( "Test event subscription/unsubscription", function( assert ) {
     }
 
     // Assert should only be called once
-    assert.expect(1);
+    assert.expect(2);
 
     // Subscribe to the event
-    niclabs.insight.event.listen('some-event', callback, subscribed);
+    niclabs.insight.event.on('some-event', callback);
     niclabs.insight.event.trigger('some-event');
 
     // Unsuscribe from the event
     subscribed = false;
-    niclabs.insight.event.listen('some-event', callback, subscribed);
+    niclabs.insight.event.off('some-event', callback);
+    niclabs.insight.event.trigger('some-event');
+
+    // Subscribe to the event and record the id
+    subscribed = true;
+    var id = niclabs.insight.event.on('some-event', callback);
+    niclabs.insight.event.trigger('some-event');
+
+    // Unsuscribe from the event
+    subscribed = false;
+    niclabs.insight.event.off('some-event', id);
     niclabs.insight.event.trigger('some-event');
 });
