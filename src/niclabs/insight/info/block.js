6f22d8332ead0dbb00affa9f088ba63bc1bbf39e
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-19 17:11:49 -0300

    Updated block to use module pattern and new dashboard structure

--- src/niclabs/insight/info/block.js
+++ src/niclabs/insight/info/block.js
@@ -1,140 +1,221 @@
-CityDashboard.Visualization = function ( props ) {
+niclabs.insight.info.Block = (function($) {
+    "use strict";
 
-var id, data_source, title, properties;
-  
-  this.id = props['id'];
+    /**
+     * Construct a information block
+     *
+     * @class niclabs.insight.info.Block
+     * @param {niclabs.insight.Dashboard} dashboard - dashboard to which the block belongs to
+     * @param {Object} options - configuration options for the block
+     * @param {string} options.id - html identifier for the block
+     * @param {string=} options.title - title for the block
+     * @param {Object=} options.properties - block properties (closable, movable)
+     */
+    var constructor = function(dashboard, options) {
+        if (!('id' in options)) {
+            throw new Error("An information block must have an id");
+        }
 
-  this.data_source = props['data-source'] || this.id;
+        var id = options.id;
+        var title = options.title || '';
+        var properties = options.properties || {};
+        var datasource = options.datasource || id; // TODO: unnecessary?
+        var preprocess = options.preprocess || function(x) {return x;};
 
-  this.properties = props['properties'] || {};
+        // placing
+        var titleElement = $('<h4>').append(title).addClass('viz-title');
 
-  this.title = props['title'] || '';
+        var container = $('<div>').setID(id).addClass('visualization')
+            .append(titleElement)
+            .append($('<hr>').addClass('viz-bar'));
 
-  this.dataPreprocess = props['preprocess'] || function (a) {return a;};
+        /**
+         * Remove the block from the dashboard.
+         * This method triggers an event to alert all elements of the
+         * dashboard of the block removal
+         *
+         * @memberof niclabs.insight.info.Block
+         */
+        function remove() {
+            // Trigger the block removal
+            niclabs.insight.event.trigger('remove-block', {
+                'id': id,
+                'datasource': datasource
+            });
+        }
 
-  this.setData( props['data'] || {});
+        // Make the block closable
+        if (properties.closable === undefined || properties.closable) {
+            container.closable(function() {
+                // Remove the block
+                remove();
+            });
+        }
 
-  //placing
+        // Make the block movable
+        if (properties.movable === undefined || properties.movable) {
+            container.movable();
+        }
 
-  var title = $( '<h4>' ).append(this.title).addClass('viz-title');
+        // TODO: not sure what this does
+        container.append($('<h6>').addClass('latlngView'));
 
-  var _this = this;
-  this.viz = $('<div>').setID( this.id ).addClass( 'visualization' )
-  .append( title )
-  .append( $('<hr>').addClass('viz-bar') );
+        // Add the properties to the block style
+        container.css(properties);
 
-  if (this.properties['closable'] === undefined || this.properties['closable']){
-    this.viz.closable(function () {return _this.remove();});
-  }
-  if (this.properties['movable'] === undefined || this.properties['movable']){
-    this.viz.movable();
-  }
+        // checkbox handling
+        // TODO: this is not a generic functionality?
+        // var checkbox_handler = options['checkbox-handler'] || function (a, d) { return d; };
+        //
+        // if (options.checkbox)
+        //     this.addCheckbox(options.checkbox);
 
-  this.viz.append( $( '<h6>' ).addClass('latlngView') );
+        // Block data
+        var data = {};
 
-  $( CityDashboard['infoWindowID'] ).append( this.viz );
+        var self = {
+            /**
+             * DOM id of the block
+             * @memberof niclabs.insight.info.Block
+             * @member {string}
+             */
+            get id () {
+                return id;
+            },
 
-  this.viz.css( this.properties );
+            /**
+             * HTML DOM element for the block container
+             *
+             * @memberof niclabs.insight.info.Block
+             * @member {Element}
+             */
+            get element () {
+                var c = $(id);
+                container = c.length === 0 ? container : c;
+                return container[0];
+            },
 
-  // checkbox handling
+            /**
+             * jQuery object for info block container
+             *
+             * @memberof niclabs.insight.info.Block
+             * @member {jQuery}
+             */
+            get $ () {
+                var c = $(id);
+                container = c.length === 0 ? container : c;
+                return container;
+            },
 
-  this.checkbox_handler = props['checkbox-handler'] || function (a,d){return d;};
+            /**
+             * Trigger the removal of the block
+             */
+            remove: remove,
 
-  if (props.checkbox)
-    this.addCheckbox( props['checkbox'] );
+            /**
+             * Set/get the data for the block
+             *
+             * @memberof niclabs.insight.info.Block
+             * @param {Object=} data - data for the block
+             * @returns {Object} the current data in the blokc
+             */
+            data: function (d) {
+                d = typeof d === 'undefined' ? data : d;
 
-};
+                // If the object is empty we cleanup the internal data
+                if (Object.keys(d).length === 0)
+                    data = {};
+                else
+                    data = preprocess(d);
 
-CityDashboard.Visualization.prototype = {
+                return data;
+            },
 
-  constructor: CityDashboard.Visualization,
-  setData: function (data) {
-    if ( !(data instanceof Array) )
-      data =  jQuery.extend({}, data);
+            refresh: function () {
+                var latlngView = self.$.find('.latlngView').empty();
 
-    if (!data || Object.keys(data).length === 0)
-      this.data = {};
-    else
-      this.data = this.dataPreprocess(data);
-  },
-  getData: function () {
-    return this.data;
-  },
-  refresh: function () {
-    
-    var latlngView = $( this.id ).find('.latlngView').empty();
+                self.$.find('.deflist').remove();
 
-    this.viz.find('.deflist').remove();
+                var data = self.getData();
 
-    var data = this.getData();
+                var lat = data.lat,
+                    lng = data.lng;
 
-    var lat = data.lat, lng = data.lng;
+                if (lat && lng)
+                    latlngView.text('lat: ' + lat + ', lng: ' + lng).insertAfter(self.$.find('hr'));
+            },
 
-    if ( lat && lng)
-      latlngView.text('lat: ' + lat + ', lng: ' + lng).insertAfter( $( this.id ).find('hr') );
-
-  },
-  remove: function () {
-    // signal infowindow to remove this viz.
-    $(CityDashboard['infoWindowID']).trigger('remove-viz',{'id':this.id,'data-source':this.data_source});
-  },
-  createDefList: function ( value ) {
-    var id = this.id;
-    $.each(value, function (key, value) {
-
-      if (key !== 'lat' && key !== 'lng' && key !=='value') {
-        $( id ).find('.deflist').append( $('<dt>').text( key).addClass('deflist-key') )
-        .append( $( '<dd>' ).text(value).addClass('deflist-value') );
-      }
-
-    });
-  },
-  addCheckbox: function ( keys ) {
-    //keys: {name1:true,name2:false,name3:true,...}
-    //each element of the object indicates the label of each checkbox. The number of keys indicates the number of checboxes.
-    //handler :  function ([true, false, true, ...],data)
-    //each element of the array corresponds to each checkbox state.
-    
-    var checkpanel = $( "<span>" ).addClass('checkbox-panel');
-
-    var arr = [];
-
-    for (var key in keys) {
-      var checkbox = $('<input>').attr('type','checkbox');
-
-      arr[arr.length] = checkbox[0].checked = keys[key];
-      checkpanel.append(checkbox);
-
-      var _this = this;
-
-      checkbox.on('change',function () {
-
-        var array = $(this).parent().children('input:checkbox').map(function(){
-          return $(this).prop('checked');
-        }).get();
-
-        _this.getData = function () {
-          var clone  = _this.data;
-          if(!(_this.data instanceof Array))
-            clone = jQuery.extend({}, _this.data);
-          return _this.checkbox_handler(array,clone);
+            /**
+             * Create a definition list from the provided data
+             *
+             * TODO: should this really go here? Change the name?
+             *
+             * @memberof niclabs.insight.info.Block
+             * @param {Object=} data - the updated data for the block
+             */
+            createDefList: function (data) {
+                $.each(data, function (key, value) {
+                    if (key !== 'lat' && key !== 'lng' && key !== 'value') {
+                        self.$.find('.deflist').append($('<dt>').text(key).addClass('deflist-key'))
+                            .append($('<dd>').text(value).addClass('deflist-value'));
+                    }
+                });
+            },
         };
 
-        _this.refresh();
-
-      })
-
-      checkbox.after( $('<label>').text(key) );
-    }
-
-    this.viz.append(checkpanel);
-
-    _this.getData = function () {
-      var clone  = _this.data;
-      if(!(_this.data instanceof Array))
-        clone = jQuery.extend({}, _this.data);
-      return _this.checkbox_handler(arr,clone);
+        return self;
     };
-  }
 
-};
+    return constructor;
+})(jQuery);
+
+// CityDashboard.Visualization.prototype = {
+//     addCheckbox: function (keys) {
+//         //keys: {name1:true,name2:false,name3:true,...}
+//         //each element of the object indicates the label of each checkbox. The number of keys indicates the number of checboxes.
+//         //handler :  function ([true, false, true, ...],data)
+//         //each element of the array corresponds to each checkbox state.
+//
+//         var checkpanel = $("<span>").addClass('checkbox-panel');
+//
+//         var arr = [];
+//
+//         for (var key in keys) {
+//             var checkbox = $('<input>').attr('type', 'checkbox');
+//
+//             arr[arr.length] = checkbox[0].checked = keys[key];
+//             checkpanel.append(checkbox);
+//
+//             var _this = this;
+//
+//             checkbox.on('change', function () {
+//
+//                 var array = $(this).parent().children('input:checkbox').map(function () {
+//                     return $(this).prop('checked');
+//                 }).get();
+//
+//                 _this.getData = function () {
+//                     var clone = _this.data;
+//                     if (!(_this.data instanceof Array))
+//                         clone = jQuery.extend({}, _this.data);
+//                     return _this.checkbox_handler(array, clone);
+//                 };
+//
+//                 _this.refresh();
+//
+//             })
+//
+//             checkbox.after($('<label>').text(key));
+//         }
+//
+//         this.viz.append(checkpanel);
+//
+//         _this.getData = function () {
+//             var clone = _this.data;
+//             if (!(_this.data instanceof Array))
+//                 clone = jQuery.extend({}, _this.data);
+//             return _this.checkbox_handler(arr, clone);
+//         };
+//     }
+//
+// };
