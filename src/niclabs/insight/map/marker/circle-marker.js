f541887a8eed4657883db16ebbf47dc9e907237b
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-24 12:08:26 -0300

    Updated circle-marker implementation

--- src/niclabs/insight/map/marker/circle-marker.js
+++ src/niclabs/insight/map/marker/circle-marker.js
@@ -1,49 +1,50 @@
-CityDashboard.CircleMarker = function( layer_params, attr, map, assoc_layer ){
+niclabs.insight.map.marker.CircleMarker = (function($) {
+    /**
+     * Constructor for circle markers
+     *
+     * Circle markers are drawn in the map as circular waypoints
+     *
+     * @class niclabs.insight.map.marker.SimpleMarker
+     * @extends niclabs.insight.map.marker.Marker
+     * @param {niclabs.insight.Dashboard} dashboard - dashboard that this marker belongs to
+     * @param {Object} options - configuration options for the layer
+     * @param {string} options.layer - identifier for the layer that this marker belongs to
+     * @params {float} options.lat - latitude for the marker
+     * @params {float} options.lng - longitude for the marker
+     * @params {string} options.description - description for the marker
+     * @params {number} [options.radius=400] - radius for the circle
+     * @params {string} [options.strokeColor='#ff0000'] - color for the circle perimenter line
+     * @params {float} [options.strokeOpacity=0.8] - opacity for the circle perimeter line
+     * @params {string} [options.fillColor='#ff0000'] - color for the circle filling
+     * @params {float} [options.fillOpacity=0.35] - opacity for the circle filling
+     */
+    var CircleMarker = function(dashboard, options) {
+        var self = niclabs.insight.map.marker.Marker(dashboard, options);
 
-  CityDashboard.Marker.call(this, layer_params, attr, map, assoc_layer);
+        var latLng = new google.maps.LatLng(parseFloat(options.lat), parseFloat(options.lng));
 
-  var myLatlng = new google.maps.LatLng( parseFloat(layer_params.lat), parseFloat(layer_params.lng) );
+        var marker = new google.maps.Circle({
+            center: latLng,
+            map: self.map.googlemap(),
+            radius: options.radius || 400,
+            strokeColor: options.strokeColor || '#FF0000',
+            strokeOpacity: options.strokeOpacity || 0.8,
+            strokeWeight: options.strokeWeight || 2,
+            fillColor: options.fillColor || '#FF0000',
+            fillOpacity: options.fillOpacity || 0.35,
+            title: options.description || ''
+        });
 
-  var circle = new google.maps.Circle({
-      center: myLatlng,
-      map: map,
-      radius: attr.radius || 400,
-      strokeColor: attr.strokeColor || '#FF0000',
-      strokeOpacity: attr.strokeOpacity || 0.8,
-      strokeWeight: attr.strokeWeight || 2,
-      fillColor: attr.fillColor || '#FF0000',
-      fillOpacity: attr.fillOpacity || 0.35,
-      title: layer_params.landmark || ''
-  });
+        // Re-write the marker function
+        self.marker = function() {
+            return marker;
+        };
 
-  this.marker = circle;
+        return self;
+    };
 
-};
+    // Register the handler
+    niclabs.insight.handler('circle-marker', 'marker', CircleMarker);
 
-CityDashboard.CircleMarker.prototype = Object.create(CityDashboard.Marker.prototype);
-
-CityDashboard.CircleMarker.prototype = {
-
-  constructor: CityDashboard.CircleMarker,
-
-  addEvents: function () {
-
-    google.maps.event.addListener(this.marker, 'click', triggerEvent);
-
-    var myself = this;
-
-    function triggerEvent() {
-
-      $('#infoWindow').trigger('marker-pressed', {'id': myself.layer.id , 'value': myself.layer_params, 'attr': myself.attr});
-
-    }
-
-  },
-
-  triggerInitialEvent: function() {
-    $('#infoWindow').trigger('marker-pressed', {'id': this.layer.id , 'value': this.layer_params, 'attr': this.attr});
-  }
-
-};
-
-
+    return CircleMarker;
+})(jQuery);
