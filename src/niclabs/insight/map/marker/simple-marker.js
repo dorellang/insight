e06d73513f38d3733d7dcfe1c5f27c9fcff5ff64
Felipe Lalanne <flalanne@niclabs.cl>
2015-03-23 23:02:22 -0300

    Fixed SimpleMarker implementation to work with the new hierarchy

--- src/niclabs/insight/map/marker/simple-marker.js
+++ src/niclabs/insight/map/marker/simple-marker.js
@@ -1,55 +1,25 @@
-CityDashboard.SimpleMarker = function( layer_params, attr, map, assoc_layer ){
+niclabs.insight.map.marker.SimpleMarker = (function($) {
+    var SimpleMarker = function(dashboard, options) {
+        var self = niclabs.insight.map.marker.Marker(dashboard, options);
 
-  CityDashboard.Marker.call(this, layer_params, attr, map, assoc_layer);
+        var latLng = new google.maps.LatLng(parseFloat(options.lat), parseFloat(options.lng));
 
-  var myLatlng = new google.maps.LatLng( parseFloat(layer_params.lat), parseFloat(layer_params.lng) );
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: self.map.googlemap(),
+            title: options.description || ''
+        });
 
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: layer_params.landmark || ''
-  });
+        // Re-write the marker function
+        self.marker = function() {
+            return marker;
+        };
 
-  this.marker = marker;
+        return self;
+    };
 
-};
+    // Register the handler
+    niclabs.insight.handler('simple-marker', 'marker', SimpleMarker);
 
-CityDashboard.SimpleMarker.prototype = Object.create(CityDashboard.Marker.prototype);
-
-CityDashboard.SimpleMarker.prototype = {
-
-  constructor: CityDashboard.SimpleMarker,
-
-  addEvents: function () {
-      
-    google.maps.event.addListener(this.marker, 'click', triggerEvent);
-
-    var myself = this;
-
-    function triggerEvent() {
-
-      $('#infoWindow').trigger('marker-pressed', {'id': myself.layer.id , 'value': myself.layer_params, 'attr': myself.attr});
-
-      for(var i = 0; i < myself.layer.markers.length; i++) {
-        var myMarker = myself.layer.markers[i].marker;
-        if (myMarker == this) {
-          myMarker.setAnimation(google.maps.Animation.BOUNCE);
-        }
-        else {
-          myMarker.setAnimation(null);
-        }
-      }
-
-    }
-
-  },
-
-  triggerInitialEvent: function() {
-    $('#infoWindow').trigger('marker-pressed', {'id': this.layer.id , 'value': this.layer_params, 'attr': this.attr});
-    for(i = 0; i < this.layer.markers.length; i++) {
-      this.layer.markers[i].marker.setAnimation(null);
-    }
-    this.marker.setAnimation(google.maps.Animation.BOUNCE);
-  }
-
-};
+    return SimpleMarker;
+})(jQuery);
