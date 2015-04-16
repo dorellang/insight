e3c203a953e33faf23e604c637a9c878577fb568
Felipe Lalanne <flalanne@niclabs.cl>
2015-04-16 16:31:40 -0300

    Added Quadtree data structure for point search

---
+++ src/niclabs/insight/quadtree/_.js
@@ -0,0 +1,15 @@
+/**
+ * Quadtree implementation
+ *
+ * @namespace
+ */
+niclabs.insight.quadtree = {};
+
+/**
+ * A cartesian point
+ *
+ * @typedef niclabs.insight.quadtree.Point
+ * @type {Object}
+ * @param {float} x - horizontal coordinates
+ * @param {float} y - vertical coordinates
+ */
