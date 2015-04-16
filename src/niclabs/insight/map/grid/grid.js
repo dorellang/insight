0fd3cb362867719a5a2733283c2132b8a8a0f338
Felipe Lalanne <flalanne@niclabs.cl>
2015-04-16 16:30:10 -0300

    Defined a Grid and GridLayers

--- src/niclabs/insight/map/grid/grid.js
+++ src/niclabs/insight/map/grid/grid.js
@@ -1,19 +1,57 @@
-CityDashboard.Grid = function( layer_params, attr, map, assoc_layer ){
+niclabs.insight.map.grid.Grid = (function() {
+	/**
+	 * Construct a grid over the map
+	 *
+	 * @class niclabs.insight.map.grid.Grid
+	 * @param {niclabs.insight.Dashboard} dashboard - dashboard that this layer belongs to
+     * @param {Object} options - configuration options for the layer
+	 */
+	var Grid = function(dashboard, options) {
+		if (!('layer' in options))
+            throw new Error('The grid must be associated to a layer');
 
-	this.layer_params = layer_params;
-	this.attr = attr;
-	this.map = map;
-	this.layer = assoc_layer;
-	this.tiles = []
+        var layer;
+        if (!(layer = dashboard.layer(options.layer)))
+            throw new Error('The layer '+layer+' does not exist in the dashboard');
 
-};
+        var map;
+        if (!(map = dashboard.map()))
+            throw new Error('No map has been initialized for the dashboard yet');
 
-CityDashboard.Grid.prototype = {
+        if (!('googlemap' in map))
+            throw new Error("Grids are only supported for Google Maps at the moment");
 
-  constructor: CityDashboard.Grid,
+        var self = {
+            /**
+             * Map view where the grid belongs to
+             * @memberof niclabs.insight.map.grid.Grid
+             * @member {niclabs.insight.MapView}
+             */
+            get map () {
+                return map;
+            },
 
-  build: function() {},
+            /**
+             * Layer to which the grid belongs to
+             *
+             * @memberof niclabs.insight.map.grid.Grid
+             * @member {niclabs.insight.layer.Layer}
+             */
+            get layer () {
+                return layer;
+            },
 
-  addEvents: function() {}
+            /**
+             * Clear the grid from the map
+             *
+             * @memberof niclabs.insight.map.grid.Grid
+             */
+            clear: function() {
+            },
+        };
 
-};
+        return self;
+	};
+
+	return Grid;
+})();
