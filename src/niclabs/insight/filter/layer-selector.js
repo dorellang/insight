882c989ed0f028c4277da1a64caf015733fffebf
Felipe Lalanne <flalanne@niclabs.cl>
2015-04-24 16:56:01 -0300

    Created a layer selector filter

---
+++ src/niclabs/insight/filter/layer-selector.js
@@ -0,0 +1,48 @@
+niclabs.insight.filter.LayerSelector = (function($) {
+
+
+    /**
+     * Construct a layer for the dashboard
+     *
+     * The layer selector provides an option to switch between layers of the dashboard
+     *
+     * @class niclabs.insight.filter.LayerSelector
+     * @augments niclabs.insight.filter.Filter
+     * @param {niclabs.insight.Dashboard} dashboard - dashboard that this filter belongs to
+     * @param {Object} options - configuration options for the filter
+     */
+    var LayerSelector = function(dashboard, options) {
+        var view = niclabs.insight.filter.Filter(dashboard, options);
+
+        var layers = {};
+
+        // Configure the view
+        var select = $('<select>');
+
+        select.on('change', function() {
+            dashboard.active($(this).val());
+        });
+
+        // Add the selector to the view
+        view.$.append(select);
+
+        /**
+         * Add a layer to the selector
+         *
+         * @memberof niclabs.insight.filter.LayerSelector
+         * @param {string} id - id for the layer
+         * @param {name} name - name of the layer
+         */
+        view.add = function(id, name) {
+            layers[id] = name;
+            select.append($('<option>').attr('value', id).text(name));
+        };
+
+        return view;
+    };
+
+    // Register the handler
+    niclabs.insight.handler('layer-selector', 'filter', LayerSelector);
+
+    return LayerSelector;
+})(jQuery);
