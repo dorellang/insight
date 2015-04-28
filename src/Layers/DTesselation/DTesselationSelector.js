16125ca619bcd84d86465f79cc8abdbead883582
Felipe Lalanne <flalanne@niclabs.cl>
2015-04-28 16:57:37 -0300

    Removed legacy source files (already replaced)

--- src/Layers/DTesselation/DTesselationSelector.js
+++
@@ -1,9 +0,0 @@
-function DTesselationSelector( layer_params, attr, map, assoc_layer ){
-
-  if ( attr.type === 'delaunay' )
-    return new CityDashboard.Delaunay( layer_params, attr, map, assoc_layer);
-
-  else if ( attr.type === 'voronoi' )
-    return new CityDashboard.Voronoi( layer_params, attr, map, assoc_layer);
-
-}
