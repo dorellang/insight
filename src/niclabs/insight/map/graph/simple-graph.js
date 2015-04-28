74011980ab72de77ddb89e38bdab45b5a0557194
fernando <nanocatdemen@gmail.com>
2015-04-28 17:49:46 -0300

    Cleanup and minor mistake

--- src/niclabs/insight/map/graph/simple-graph.js
+++ src/niclabs/insight/map/graph/simple-graph.js
@@ -1,80 +1,96 @@
 niclabs.insight.map.graph.SimpleGraph = (function($) {
+  /**
+   * TODO: Missing documentation
+   */
+  var SimpleGraph = function(dashboard, options) {
+    if (!('data' in options)) {
+      throw Error('No data provided for the graph');
+    }
+
+    var self = niclabs.insight.map.graph.Graph(dashboard, options);
+
     /**
      * TODO: Missing documentation
      */
-    var SimpleGraph = function(dashboard, options) {
-        if (!('data' in options)) {
-            throw Error('No data provided for the graph');
+    function googleMapsGraph(data) {
+      var graphNodes = [];
+      for (var i = 0; i < data.length; i++) {
+        var latLng = new google.maps.LatLng(data[i].lat, data[i].lng);
+        var marker = new google.maps.Marker({
+          position: latLng,
+          map: self.map.googlemap(),
+          title: data[i].landmark || ''
+        });
+        graphNodes.push(marker);
+      }
+      var vertex = [];
+
+      /*
+      changeColor = function() {
+          // TODO: make configurable?
+          polyline.setOptions({strokeColor: '#FF0000'});
+
+          // Set timeout to stop the animation
+          setTimeout(function() {
+              polyline.setOptions({strokeColor: '#000000'});
+          }, 3000);
+       };
+       */
+
+      // Note: non directed graph
+      for (i = 0; i < options.adj.length; i++) {
+        for (var j = 0; j < i; j++) {
+          if (options.adj[i][j] == 1) {
+            var myLatlngArray = [];
+            myLatlngArray[0] = new google.maps.LatLng(data[i].lat, data[i].lng);
+            myLatlngArray[1] = new google.maps.LatLng(data[j].lat, data[j].lng);
+
+            var polyline = new google.maps.Polyline({
+              path: myLatlngArray,
+              strokeColor: '#000000',
+              strokeOpacity: 1.0,
+              strokeWeight: 10
+            });
+
+            polyline.setMap(self.map.googlemap());
+            vertex.push(polyline);
+          }
         }
+      }
 
-        var self = niclabs.insight.map.graph.Graph(dashboard, options);
+      return {
+        nodes: graphNodes,
+        vertex: vertex
+      };
+    }
 
-        /**
-         * TODO: Missing documentation
-         */
-        function googleMapsGraph(data) {
-            var graphNodes = [];
-            for (var i = 0; i < data.length; i++) {
-                var latLng = new google.maps.LatLng(data[i].lat, data[i].lng);
-                var marker = new google.maps.Marker({
-                    position: latLng,
-                    map: self.map.googlemap(),
-                    title: data[i].landmark || ''
-                });
-                graphNodes.push(marker);
-            }
-            var vertex = [];
-            // Note: non directed graph
-            for (i = 0; i < options.adj.length; i++) {
-                for (var j = 0; j < i; j++) {
-                    if (options.adj[i][j] == 1) {
-                        var myLatlngArray = [];
-                        myLatlngArray[0] = new google.maps.LatLng(data[i].lat, data[i].lng);
-                        myLatlngArray[1] = new google.maps.LatLng(data[j].lat, data[j].lng);
+    // Create the graph
+    var graph = googleMapsGraph(options.data);
 
-                        var path = new google.maps.Polyline({
-                          path: myLatlngArray,
-                          strokeColor: '#000000',
-                          strokeOpacity: 1.0,
-                          strokeWeight: 2
-                        });
+    // Set the options
 
-                        path.setMap(self.map.googlemap());
-                        vertex.push(path);
-                    }
-                }
-            }
+    // Set the graph
+    //graph.setMap(self.map.googlemap());
 
-            return {'nodes': graphNodes, 'vertex': vertex};
-        }
+    // Store the parent
+    var clear = self.clear;
 
-        // Create the graph
-        var graph = googleMapsGraph(options.data);
+    /**
+     * TODO: Missing documentation
+     */
+    self.clear = function() {
+      // Call the parent
+      clear();
 
-        // Set the options
-
-        // Set the graph
-        //graph.setMap(self.map.googlemap());
-
-        // Store the parent
-        var clear = self.clear;
-
-        /**
-         * TODO: Missing documentation
-         */
-        self.clear = function() {
-            // Call the parent
-            clear();
-
-            // Remove the map
-            graph.setMap(null);
-        };
-
-        return self;
+      // Remove the map
+      graph.setMap(null);
     };
 
-    // Register the handler
-    niclabs.insight.handler('simple-graph', 'graph', SimpleGraph);
+    return self;
+  };
 
-    return SimpleGraph;
+  // Register the handler
+  niclabs.insight.handler('simple-graph', 'graph', SimpleGraph);
+
+  return SimpleGraph;
 })(jQuery);
