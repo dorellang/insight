e8b5ff50a7f4c0fbcf0d8903bd99c22e0cf46332
fernando <nanocatdemen@gmail.com>
2015-06-09 12:31:48 -0300

    The dashboard listen to url hash changes Layer selector and filter can be loaded now by url. Refactoring needed.

--- src/niclabs/insight/dashboard.js
+++ src/niclabs/insight/dashboard.js
@@ -83,12 +83,16 @@ niclabs.insight.Dashboard = (function($) {
 
         // Listen to hash changes
         niclabs.insight.event.on('hash', function() {
-            if (window.location.hash.includes('filter=')) {
-                var hashValue = window.location.hash;
-                var f = window.location.hash.slice(hashValue.indexOf('filter=')+7);
+            var hashValue = window.location.hash;
+            if (hashValue.includes('filter/')) {
+                var f = hashValue.slice(hashValue.indexOf('filter/')+7);
                 // Eval is evil
                 currentFilter = new Function('data','return ' + f);
                 activeLayer.filter(currentFilter);
+            }
+            if (hashValue.includes('layer/')) {
+                var layerId = hashValue.slice(hashValue.indexOf('layer/')+6);
+                self.active(layerId);
             }
         });
 
@@ -96,6 +100,7 @@ niclabs.insight.Dashboard = (function($) {
             niclabs.insight.event.trigger('hash');
         });
 
+        // This should be improved
         $(window).on('load', function() {
             niclabs.insight.event.trigger('hash');
         });
